fa9418dc3e92f0efb3f4fad0255261afd0116d1f
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-16 22:19:52 -0600

    add comment functionality and change color when votes are positive or negative

---
+++ readme.md
@@ -0,0 +1,7 @@
+## TO DO:
+
+ - Form Validation
+ - Time stamps
+ - Ability to add Comments
+ - Ability to show/hide Comments
+ - number of comments
--- src/controllers.js
+++ src/controllers.js
@@ -8,13 +8,48 @@
   app.controller('searchArticlesController', searchArticlesController)
 
   function subgreenitController ($rootScope) {
+
+    this.showComments = function (article) {
+      if (article.commentView === true) {
+        article.commentView = false
+      } else {
+        article.commentView = true
+      }
+    }
+
+    this.showCommentForm = function (article) {
+      if (article.commentFormView === true) {
+        article.commentFormView = false
+      } else {
+        article.commentFormView = true
+      }
+    }
+
+    this.addComment = function (article) {
+      article.commentList.push(this.commentData)
+      this.commentData = {
+        author: '',
+        content: ''
+      }
+      article.commentFormView = false
+    }
+
     $rootScope.showPost = false
+
+    this.commentData = {
+      author: '',
+      content: ''
+    }
 
     this.newPostData = {
       title: '',
       author: '',
       image: '',
-      description: ''
+      description: '',
+      votes: 0,
+      commentList: [],
+      commentView: false,
+      commentFormView: false
     }
 
     this.postArray = [{
@@ -22,29 +57,42 @@
       author: 'James Baxter',
       image: 'https://hd.unsplash.com/25/peak.jpg',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      votes: 0
+      votes: 0,
+      commentList: [{author: 'Jimmy', content: `This is dumb`}, {author: 'Sarah', content: `No it's not, it's awesome...`}, {author: 'Mack', content: `You guys are weird...`}],
+      visible: false
     },{
       title: 'Attatude During the Times',
       author: 'Sam Gam',
       image: 'https://hd.unsplash.com/photo-1445325693269-a5912d1dbc05',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      votes: 0
+      votes: 0,
+      commentList: [{author: 'Jimmy', content: `This is dumb`}, {author: 'Sarah', content: `No it's not, it's awesome...`}, {author: 'Mack', content: `You guys are weird...`}],
+      commentView: false,
+      commentFormView: false
     },{
       title: 'Quintuplets During the Times',
       author: 'Tori Morrison',
       image: 'https://hd.unsplash.com/photo-1444310689954-f041cfbe29f2',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      votes: 0
+      votes: 0,
+      commentList: [{author: 'Jimmy', content: `This is dumb`}, {author: 'Sarah', content: `No it's not, it's awesome...`}, {author: 'Mack', content: `You guys are weird...`}],
+      commentView: false,
+      commentFormView: false
     }]
 
     this.addPost = function () {
+
       this.postArray.push(this.newPostData)
       $rootScope.showPost = false
       this.newPostData = {
         title: '',
         author: '',
         image: '',
-        description: ''
+        description: '',
+        votes: 0,
+        commentList: [],
+        commentView: false,
+        commentFormView: false
       }
     }
     this.downVote = function (article) {
@@ -66,9 +114,11 @@
   }
 
   function sortArticlesController ($rootScope) {
+    this.selection = "-votes"
+    $rootScope.selection = this.selection
     this.sort = function () {
       $rootScope.selection = this.selection
-      console.log($rootScope.selection);
+      console.log(this.selection);
     }
   }
 
@@ -77,5 +127,6 @@
       $rootScope.searchText = this.searchText
     }
   }
+
 
 }());
